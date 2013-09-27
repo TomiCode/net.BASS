003676b8c16c90538aeadc3117b8940dc973f6c3
TomiCode <tomicode@gmail.com>
2013-09-27 19:56:24 +0200

    Changed handle to INT and added error enum.

--- net.BASS/BASS.cs
+++ net.BASS/BASS.cs
@@ -7,42 +7,52 @@ namespace netBASS
     {
         [DllImport(@"bass.dll", CharSet = CharSet.Auto)]
         [return: MarshalAs(UnmanagedType.Bool)]
-        public static extern bool BASS_Init(int device, uint freq, BASSInit flags, IntPtr win, IntPtr clsid);
+        public static extern bool BASS_Init(int device, int freq, BASSInit flags, IntPtr win, IntPtr clsid);
+
+        public static bool BASS_Init(int device, int freq, BASSInit flags, IntPtr win)
+        {
+            return BASS_Init(device, freq, flags, win, IntPtr.Zero);
+        }
 
         [DllImport(@"bass.dll", EntryPoint = "BASS_StreamCreateFile", CharSet = CharSet.Auto)]
-        private static extern uint BASS_SCF([MarshalAs(UnmanagedType.Bool)]bool mem, [MarshalAs(UnmanagedType.LPStr)]string file, long offset, long lenght, BASSFlag flags);
+        private static extern int BASS_SCF([MarshalAs(UnmanagedType.Bool)]bool mem, [MarshalAs(UnmanagedType.LPStr)]string file, long offset, long lenght, BASSFlag flags);
 
-        public static uint BASS_StreamCreateFile(string file, long offset, long lenght, BASSFlag flags)
+        public static int BASS_StreamCreateFile(string file, long offset, long lenght, BASSFlag flags)
         {
             return BASS_SCF(false, file, offset, lenght, flags);
         }
 
         [DllImport(@"bass.dll", CharSet = CharSet.Auto)]
-        public static extern bool BASS_ChannelPlay(uint handle, [MarshalAs(UnmanagedType.Bool)]bool restart);
+        public static extern bool BASS_ChannelPlay(int handle, [MarshalAs(UnmanagedType.Bool)]bool restart);
 
         [DllImport(@"bass.dll", CharSet = CharSet.Auto)]
-        public static extern int BASS_ErrorGetCode();
+        public static extern BASSError BASS_ErrorGetCode();
 
         [DllImport(@"bass.dll", CharSet = CharSet.Auto)]
         [return: MarshalAs(UnmanagedType.Bool)]
-        public static extern bool BASS_MusicFree(uint handle);
+        public static extern bool BASS_MusicFree(int handle);
 
         [DllImport(@"bass.dll", EntryPoint = "BASS_MusicLoad", CharSet = CharSet.Auto)]
-        private static extern uint BASS_ML([MarshalAs(UnmanagedType.Bool)]bool mem, IntPtr file,
-            long offset, int lenght, BASSFlag flags, uint freq);
+        private static extern int BASS_ML([MarshalAs(UnmanagedType.Bool)]bool mem, IntPtr file, long offset, int lenght, BASSFlag flags, int freq);
 
-        public static uint BASS_MusicLoad(string file, long offset, int lenght, BASSFlag flags, uint freq)
+        public static int BASS_MusicLoad(string file, long offset, int lenght, BASSFlag flags, int freq)
         {
             IntPtr ptr = Marshal.StringToHGlobalAnsi(file);
-            return BASS_ML(false, ptr, offset, lenght, flags, freq); 
+            int handle = BASS_ML(false, ptr, offset, lenght, flags, freq);
+            Marshal.FreeHGlobal(ptr);
+
+            return handle;
         }
 
-        public static uint BASS_MusicLoad(byte[] file, long offset, int lenght, BASSFlag flags, uint freq)
+        public static int BASS_MusicLoad(byte[] file, long offset, int lenght, BASSFlag flags, int freq)
         {
             IntPtr ptr = Marshal.AllocHGlobal(file.Length);
             Marshal.Copy(file, 0, ptr, file.Length);
 
-            return BASS_ML(true, ptr , offset, file.Length, flags, freq);
+            int handle = BASS_ML(true, ptr , offset, file.Length, flags, freq);
+            Marshal.FreeHGlobal(ptr);
+
+            return handle;
         }
 
         [DllImport(@"bass.dll", CharSet = CharSet.Auto)]
--- net.BASS/BASSFlags.cs
+++ net.BASS/BASSFlags.cs
@@ -7,38 +7,39 @@ namespace netBASS
 {
     public enum BASSInit
     {
-        BASS_DEVICE_8BITS = 1,		// 8 bit resolution, else 16 bit
-        BASS_DEVICE_MONO = 2,		// mono, else stereo
-        BASS_DEVICE_3D = 4,		// enable 3D functionality
-        BASS_DEVICE_LATENCY = 0x100,	// calculate device latency (BASS_INFO struct)
-        BASS_DEVICE_CPSPEAKERS = 0x400,	// detect speakers via Windows control panel
-        BASS_DEVICE_SPEAKERS = 0x800,	// force enabling of speaker assignment
-        BASS_DEVICE_NOSPEAKER = 0x1000,	// ignore speaker arrangement
-        BASS_DEVICE_DMIX = 0x2000,	// use ALSA "dmix" plugin
-        BASS_DEVICE_FREQ = 0x4000	// set device sample rate
+        BASS_DEVICE_DEFAULT     = 0,  // Device default
+        BASS_DEVICE_8BITS       = 1,  // 8 bit resolution, else 16 bit
+        BASS_DEVICE_MONO        = 2,  // mono, else stereo
+        BASS_DEVICE_3D          = 4,  // enable 3D functionality
+        BASS_DEVICE_LATENCY     = 0x100,  // calculate device latency (BASS_INFO struct)
+        BASS_DEVICE_CPSPEAKERS  = 0x400,  // detect speakers via Windows control panel
+        BASS_DEVICE_SPEAKERS    = 0x800,  // force enabling of speaker assignment
+        BASS_DEVICE_NOSPEAKER   = 0x1000, // ignore speaker arrangement
+        BASS_DEVICE_DMIX        = 0x2000, // use ALSA "dmix" plugin
+        BASS_DEVICE_FREQ        = 0x4000  // set device sample rate
     }
 
     public enum BASSFlag
     {
-        BASS_DEFAULT            = 0,    // Default settings.
-        BASS_SAMPLE_8BITS       = 1,	// 8 bit
-        BASS_SAMPLE_MONO        = 2,	// mono
-        BASS_SAMPLE_LOOP        = 4,	// looped
-        BASS_SAMPLE_3D          = 8,	// 3D functionality
-        BASS_SAMPLE_SOFTWARE    = 16,	// not using hardware mixing
-        BASS_SAMPLE_MUTEMAX     = 32,	// mute at max distance (3D only)
-        BASS_SAMPLE_VAM         = 64,	// DX7 voice allocation & management
-        BASS_SAMPLE_FX          = 128,  // old implementation of DX8 effects
-        BASS_SAMPLE_FLOAT       = 256,	// 32-bit floating-point
+        BASS_DEFAULT            = 0,  // Default settings.
+        BASS_SAMPLE_8BITS       = 1,  // 8 bit
+        BASS_SAMPLE_MONO        = 2,  // mono
+        BASS_SAMPLE_LOOP        = 4,  // looped
+        BASS_SAMPLE_3D          = 8,  // 3D functionality
+        BASS_SAMPLE_SOFTWARE    = 16,  // not using hardware mixing
+        BASS_SAMPLE_MUTEMAX     = 32,  // mute at max distance (3D only)
+        BASS_SAMPLE_VAM         = 64,  // DX7 voice allocation & management
+        BASS_SAMPLE_FX          = 128, // old implementation of DX8 effects
+        BASS_SAMPLE_FLOAT       = 256, // 32-bit floating-point
         BASS_SAMPLE_OVER_VOL    = 0x10000,	// override lowest volume
         BASS_SAMPLE_OVER_POS    = 0x20000,	// override longest playing
-        BASS_SAMPLE_OVER_DIST   = 0x30000,   // override furthest from listener (3D only)
+        BASS_SAMPLE_OVER_DIST   = 0x30000,  // override furthest from listener (3D only)
 
-        BASS_STREAM_PRESCAN     = 0x20000, // enable pin-point seeking/length (MP3/MP2/MP1)
+        BASS_STREAM_PRESCAN     = 0x20000,  // enable pin-point seeking/length (MP3/MP2/MP1)
         BASS_MP3_SETPOS	        = BASS_STREAM_PRESCAN,
         BASS_STREAM_AUTOFREE    = 0x40000,	// automatically free the stream when it stop/ends
         BASS_STREAM_RESTRATE    = 0x80000,	// restrict the download rate of internet file streams
-        BASS_STREAM_BLOCK       = 0x100000,// download/play internet file stream in small blocks
+        BASS_STREAM_BLOCK       = 0x100000, // download/play internet file stream in small blocks
         BASS_STREAM_DECODE      = 0x200000, // don't play the stream, only decode (BASS_ChannelGetData)
         BASS_STREAM_STATUS      = 0x800000, // give server status info (HTTP/ICY tags) in DOWNLOADPROC
 
@@ -49,7 +50,7 @@ namespace netBASS
         BASS_MUSIC_FX           = BASS_SAMPLE_FX,
         BASS_MUSIC_AUTOFREE     = BASS_STREAM_AUTOFREE,
         BASS_MUSIC_DECODE       = BASS_STREAM_DECODE,
-        BASS_MUSIC_PRESCAN      = BASS_STREAM_PRESCAN,	// calculate playback length
+        BASS_MUSIC_PRESCAN      = BASS_STREAM_PRESCAN,  // calculate playback length
         BASS_MUSIC_CALCLEN      = BASS_MUSIC_PRESCAN,
         BASS_MUSIC_RAMP         = 0x200,	// normal ramping
         BASS_MUSIC_RAMPS        = 0x400,	// sensitive ramping
@@ -64,7 +65,6 @@ namespace netBASS
         BASS_MUSIC_STOPBACK     = 0x80000,	// stop the music on a backwards jump effect
         BASS_MUSIC_NOSAMPLE     = 0x100000, // don't load the samples
 
-        // Speaker assignment flags
         BASS_SPEAKER_FRONT      = 0x1000000,	// front speakers
         BASS_SPEAKER_REAR       = 0x2000000,	// rear/side speakers
         BASS_SPEAKER_CENLFE     = 0x3000000,	// center & LFE speakers (5.1)
